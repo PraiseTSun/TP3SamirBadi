e43c5acf13734fe048f4f6dd2ebdfb8cb2b2ce1b
Samir Badi <samirbadi1@outlook.com>
2022-04-14 17:27:45 -0400

    Add the methods createAdmin to AdminService

--- src/main/java/javatown/service/AdminService.java
+++ src/main/java/javatown/service/AdminService.java
@@ -1,14 +1,28 @@
 package javatown.service;
 
+import javatown.DTO.AdminFormDTO;
+import javatown.modele.Administrator;
+import javatown.repository.AdminRepository;
 import javatown.repository.ClientRepository;
 import javatown.repository.DebtRepository;
 import javatown.repository.LoanRepository;
 
 public class AdminService extends AbstractCommunService{
-    private AdminService adminService;
+    private AdminRepository adminRepository;
 
-    public AdminService(ClientRepository clientRepository, LoanRepository loanRepository, DebtRepository debtRepository, AdminService adminService) {
+    public AdminService(ClientRepository clientRepository, LoanRepository loanRepository, DebtRepository debtRepository, AdminRepository adminRepository) {
         super(clientRepository, loanRepository, debtRepository);
-        this.adminService = adminService;
+        this.adminRepository = adminRepository;
     }
+
+    public AdminFormDTO createAdmin(String firstName, String lastName, String password) {
+        return createAdmin(new Administrator(firstName, lastName, password));
+    }
+
+    public AdminFormDTO createAdmin(Administrator admin){
+        adminRepository.save(admin);
+        return new AdminFormDTO(admin);
+    }
+
+    
 }
