86dfc0b0408ff8d4837ba75b86d4fb737d99e747
Samir Badi <samirbadi1@outlook.com>
2022-04-14 16:36:56 -0400

    Add the method findByIdWithLoans to ClientRepository

--- src/main/java/javatown/repository/ClientRepository.java
+++ src/main/java/javatown/repository/ClientRepository.java
@@ -8,5 +8,13 @@ import org.springframework.data.repository.query.Param;
 import java.util.Optional;
 
 public interface ClientRepository extends JpaRepository<Client, Long>{
+    @Query(value = "SELECT p FROM Client p " +
+            "LEFT JOIN FETCH p.loans " +
+            "WHERE p.id = :clientId")
+    Optional<Client> findByIdWithLoans(@Param("clientId") Long clientId);
+    @Query(value = "SELECT p FROM Client p " +
+            "LEFT JOIN FETCH p.loans " +
+            "WHERE p.firstName = :firstName AND p.lastName = :lastName AND p.password = :password")
+    Optional<Client> findByIdWithLoans(@Param("firstName") String firstName,@Param("lastName") String lastName,@Param("password") String password);
 
 }
