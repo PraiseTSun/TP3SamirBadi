3cabb15dbd1ab6afc88c400cf3122458a378ea4d
Samir Badi <samirbadi1@outlook.com>
2022-04-14 16:38:24 -0400

    Create the interface DebtRepository

---
+++ src/main/java/javatown/repository/DebtRepository.java
@@ -0,0 +1,13 @@
+package javatown.repository;
+
+import javatown.modele.Debt;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DebtRepository extends JpaRepository<Debt, Long> {
+
+}
