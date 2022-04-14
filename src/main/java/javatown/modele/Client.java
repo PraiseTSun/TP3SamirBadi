c86ee0085ea905197578db614b8a8c8765b46444
Samir Badi <samirbadi1@outlook.com>
2022-04-14 16:30:46 -0400

    Create the class Client

---
+++ src/main/java/javatown/modele/Client.java
@@ -0,0 +1,51 @@
+package javatown.modele;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@ToString(callSuper = true)
+public class Client extends AbstractUser{
+    private String resident;
+    @OneToMany(mappedBy = "client")
+    private List<Loan> loans;
+    @OneToMany(mappedBy = "client")
+    private List<Debt> debts;
+
+    public Client(String firstName, String lastName, String password, String resident) {
+        super(firstName, lastName, password);
+        this.resident = resident;
+        this.loans = new ArrayList<>();
+        this.debts = new ArrayList<>();
+    }
+
+    public boolean isReseidentOfJavatown(){
+        return resident.equals("Javatows");
+    }
+
+    public void addLoan(Loan loan) {
+        loans.add(loan);
+    }
+
+    public Loan getLoanOfId(long loanId) {
+        for (Loan loan: loans) {
+            if(loan.getId() == loanId) return loan;
+        }
+        return null;
+    }
+
+    public Debt getDebtById(long debtId) {
+        for (Debt debt: debts) {
+            if(debt.getId() == debtId) return debt;
+        }
+        return null;
+    }
+}
