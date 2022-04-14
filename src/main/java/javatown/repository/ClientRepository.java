992ce7cd314db0163fc3ea0097f2410736b3c74c
Samir Badi <samirbadi1@outlook.com>
2022-04-14 16:35:56 -0400

    Create the interface ClientRepository

---
+++ src/main/java/javatown/repository/ClientRepository.java
@@ -0,0 +1,12 @@
+package javatown.repository;
+
+import javatown.modele.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface ClientRepository extends JpaRepository<Client, Long>{
+
+}
