614c0a0e5aa44ca5390f213d82ad32c8d9559536
Samir Badi <samirbadi1@outlook.com>
2022-04-14 16:42:01 -0400

    Add the method findAllByAuthor to DocumentRepository

--- src/main/java/javatown/repository/DocumentRepository.java
+++ src/main/java/javatown/repository/DocumentRepository.java
@@ -13,4 +13,6 @@ public interface DocumentRepository extends JpaRepository<AbstractDocument, Long
     @Query(value = "SELECT p FROM AbstractDocument p " +
             "WHERE p.title LIKE %:title%")
     Optional<List<AbstractDocument>> findByTitle(@Param("title") String title);
+    Optional<List<AbstractDocument>> findAllByAuthor(String author);
+
 }
