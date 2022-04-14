281a95d1053b0d8db836d4e80bfab7d389569cc9
Samir Badi <samirbadi1@outlook.com>
2022-04-14 17:31:22 -0400

    Add the methods createClient to ClientService

--- src/main/java/javatown/service/ClientService.java
+++ src/main/java/javatown/service/ClientService.java
@@ -1,5 +1,7 @@
 package javatown.service;
 
+import javatown.DTO.ClientFormDTO;
+import javatown.modele.Client;
 import javatown.repository.ClientRepository;
 import javatown.repository.DebtRepository;
 import javatown.repository.DocumentRepository;
@@ -13,5 +15,14 @@ public class ClientService extends AbstractCommunService{
     public ClientService(ClientRepository clientRepository, LoanRepository loanRepository, DebtRepository debtRepository, DocumentRepository documentRepository) {
         super(clientRepository, loanRepository, debtRepository);
         this.documentRepository = documentRepository;
+    }
+
+    public ClientFormDTO createClient(String firstName, String lastName, String password, String resident) {
+        return createClient(new Client(firstName, lastName, password, resident));
+    }
+
+    public ClientFormDTO createClient(Client client) {
+        clientRepository.save(client);
+        return new ClientFormDTO(client);
     }
 }
