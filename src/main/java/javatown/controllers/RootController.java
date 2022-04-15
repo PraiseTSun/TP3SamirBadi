b77aafd26ba49c21c1919035ff609d1ae23158b9
Samir Badi <samirbadi1@outlook.com>
2022-04-14 21:55:53 -0400

    Add the PostMapping clientPost

--- src/main/java/javatown/controllers/RootController.java
+++ src/main/java/javatown/controllers/RootController.java
@@ -8,8 +8,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -44,5 +46,13 @@ public class RootController {
         clientForm = new ClientFormDTO();
         model.addAttribute("clientForm", clientForm);
         return "clientedit";
+    }
+
+    @PostMapping("/clientcreate")
+    public String clientPost(@ModelAttribute ClientFormDTO clientForm, Model model, BindingResult errors, RedirectAttributes redirectAttributes){
+        logger.info("client:" + clientForm);
+        clientService.createClient(clientForm.toModel());
+        redirectAttributes.addFlashAttribute("clientForm", clientForm);
+        return "clientresult";
     }
 }
