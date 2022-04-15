3d8b967456df5c86fc3c76abfaf35e178512debe
Samir Badi <samirbadi1@outlook.com>
2022-04-14 21:54:21 -0400

    Add the GetMapping getRootRequest

--- src/main/java/javatown/controllers/RootController.java
+++ src/main/java/javatown/controllers/RootController.java
@@ -6,6 +6,8 @@ import javatown.service.EmployeeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RootController {
@@ -19,5 +21,12 @@ public class RootController {
         this.clientService = clientService;
         this.employeeService = employeeService;
         this.adminService = adminService;
+    }
+
+    @GetMapping("/")
+    public String getRootRequest(Model model){
+        model.addAttribute("pageTitle", "TP3 Samir Badi");
+        model.addAttribute("mainTitle", "Users Sections");
+        return "index";
     }
 }
