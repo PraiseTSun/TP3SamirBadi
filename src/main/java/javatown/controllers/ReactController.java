9375c5899c30ffa3aac74182496cb25234643fb8
Samir Badi <samirbadi1@outlook.com>
2022-05-10 12:28:41 -0400

    Create the Controller for REACT

---
+++ src/main/java/javatown/controllers/ReactController.java
@@ -0,0 +1,27 @@
+package javatown.controllers;
+
+import javatown.DTO.*;
+import javatown.service.AdminService;
+import javatown.service.ClientService;
+import javatown.service.EmployeeService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/react")
+public class ReactController {
+    private AdminService adminService;
+    private ClientService clientService;
+    private EmployeeService employeService;
+
+    public ReactController(AdminService adminService, ClientService clientService, EmployeeService employeeService) {
+        this.adminService = adminService;
+        this.clientService = clientService;
+        this.employeService = employeService;
+    }
+
+}
